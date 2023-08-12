6793d7434fbbe187c0869133593c8c41eca850b4
Francisco Alvizo <froylan_alvizo@hotmail.com>
2023-08-11 18:29:06 -0600

    Añadido el dialogo de finalizar

--- src/App.java
+++ src/App.java
@@ -2,37 +2,44 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
+
         ElegirMonedaFuncion fun = new ElegirMonedaFuncion();
         Temperatura tem = new Temperatura();
+        int conf = 0;
+        while (conf == 0) {
 
-        Object[] opcionesDeConversion = { "Conversor de Moneda", "Conversor de Temperatura" };
+            Object[] opcionesDeConversion = { "Conversor de Moneda", "Conversor de Temperatura" };
 
-        Object opcion = JOptionPane.showInputDialog(null, "Seleccione una opción de conversión: ", "Menu",
-                JOptionPane.PLAIN_MESSAGE, null, opcionesDeConversion, opcionesDeConversion[0]);
+            Object opcion = JOptionPane.showInputDialog(null, "Seleccione una opción de conversión: ", "Menu",
+                    JOptionPane.PLAIN_MESSAGE, null, opcionesDeConversion, opcionesDeConversion[0]);
 
-        System.out.println(opcion);
+            if (opcion == "Conversor de Moneda") {
 
-        if (opcion == "Conversor de Moneda") {
+                String valor = JOptionPane.showInputDialog(null, "Ingresa la cantidad de dinero que deseas convertir: ",
+                        "Valor", JOptionPane.QUESTION_MESSAGE);
 
-            String valor = JOptionPane.showInputDialog(null, "Ingresa la cantidad de dinero que deseas convertir: ",
-                    "Valor", JOptionPane.QUESTION_MESSAGE);
+                while (!EsNumero(valor)) {
+                    valor = JOptionPane.showInputDialog(null, "Por favor inserte un numero valido ", "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
 
-            while (!EsNumero(valor)) {
-                valor = JOptionPane.showInputDialog(null, "Por favor inserte un numero valido ", "Error",
-                        JOptionPane.ERROR_MESSAGE);
+                fun.elegirmoneda(valor);
+
+            } else if (opcion == "Conversor de Temperatura") {
+                String valor = JOptionPane.showInputDialog(null, "Ingrese la temperatura que desea convertir", "Valor",
+                        JOptionPane.QUESTION_MESSAGE);
+                while (!EsNumero(valor)) {
+                    valor = JOptionPane.showInputDialog(null, "Por favor inserte un numero valido ", "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                tem.TipoDeGrados(valor);
             }
+            conf = JOptionPane.showConfirmDialog(null, "¿Desea continuar?", "Selecciona una opcion", 1);
+            System.out.println(conf);
 
-            fun.elegirmoneda(valor);
-
-        } else if (opcion == "Conversor de Temperatura") {
-            String valor = JOptionPane.showInputDialog(null, "Ingrese la temperatura que desea convertir", "Valor",
-                    JOptionPane.QUESTION_MESSAGE);
-            while (!EsNumero(valor)) {
-                valor = JOptionPane.showInputDialog(null, "Por favor inserte un numero valido ", "Error",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-            tem.TipoDeGrados(valor);
         }
+
+        JOptionPane.showMessageDialog(null, "Programa Terminado", "Message", JOptionPane.INFORMATION_MESSAGE);
 
     }
 
